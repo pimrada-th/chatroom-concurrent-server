15b7ac41a183ea32fb142fba369e7a00db7821ac
pimrada-th <pimrada.pengsook@gmail.com>
2020-10-08 03:33:33 +0700

    check online users

--- cc.py
+++ cc.py
@@ -1,17 +1,44 @@
 #Coded by Yashraj Singh Chouhan
-import socket, threading, sys
-nickname = input("What's your Nickname?: ")
-host = '127.0.0.1'                                                     #LocalHost
-port = 8899   
-user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      #socket initialization
-user.connect((host, port))                             #connecting user to server
+import socket, threading, sys, datetime, pickle
+
+date = datetime.datetime.now()
+timenow = date.strftime('%X')
+def menulist():
+    print("""
+        ******** Welcome to Mara's Chatroom ********
+            These are command that you can use in Chatroom
+            #show or #s- for show all online users
+            #nickname - direct message Example : #ken = direct message to Ken
+            #chatroom - back to chatroom
+            #help or #h - show all menu
+            #exit or #e- exit from this Chatroom
+        ******** Hope you enjoy with my program! ********""")
+
+menulist()
+while True:
+    nickname = input("What's your Nickname?: ")
+    if nickname == '#show' or nickname == '#s' or nickname == '#help' or nickname == '#h' or nickname == '#show' or nickname == '#exit' or nickname == '#e': #check name
+         print("You can't use command to be name, Please try again")              
+    else:
+        host = '127.0.0.1'                                                     #LocalHost
+        port = 8899   
+        user = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      #socket initialization
+        user.connect((host, port))                                      #connecting user to server
+        break    
+
 
 def receive():
     while True:                                                 #making valid connection
         try:
             message = user.recv(1024).decode('utf-8')
-            if message == 'NICKNAME': #ส่งแชทส่วนตัว
-                user.send(nickname.encode('utf-8'))
+            if message == 'NICKNAME': #ถ้า NICKNAME ตรงก็ให้ส่งชื่อไปที่เซิฟ
+                user.send(nickname.encode('utf-8')) #send nick name
+            #elif message == 'SHOW':
+                #listnames = pickle.loads(message)
+                #listnames = eval(message)
+                #print('Online users now : '+listnames)
+                #receive_thread = threading.Thread(target=receive) 
+                #receive_thread.start()  
             else:
                 print(message)        
         except :    #case on wrong ip/port details
@@ -22,7 +49,7 @@ def receive():
 def write():
     while True:
         typing = input("Typing : ")       
-        if typing=='#exit' :
+        if typing=='#exit' or typing == "#e" :
             check = input('Do you want to exit this chat room? (y,Y = yes, anything = no): ')
             if(check=='y' or check == 'Y' ): 
                 user.send('#exit'.encode('utf-8'))         #ถ้าไม่มีมันจะไม่หยุดทำงงาน
@@ -33,9 +60,19 @@ def write():
                 print("You back to chat room") 
                 receive_thread = threading.Thread(target=receive)                  #sending messages 
                 receive_thread.start()     
-                  
+        
+        elif typing == '#help' or typing == '#h':
+            menulist()
+            receive_thread = threading.Thread(target=receive)                  #sending messages 
+            receive_thread.start()   
+        
+        elif typing == '#show' or typing == '#s':     
+            user.send('#show'.encode('utf-8'))
+            print("--- Online Users ---")
+            receive_thread = threading.Thread(target=receive)                  #sending messages 
+            receive_thread.start()   
         else:
-            message = '{}0: {}'.format(nickname, typing)          #message layout
+            message = '{} [{}]: {}'.format(nickname, timenow, typing)          #message layout
             user.send(message.encode('utf-8'))   
 
 receive_thread = threading.Thread(target=receive)               #receiving multiple messages
--- cs.py
+++ cs.py
@@ -1,5 +1,5 @@
 #Coded by Yashraj Singh Chouhan
-import socket, threading                                                #Libraries import
+import socket, threading, datetime, pickle                                                #Libraries import
 
 host = '127.0.0.1'                                                      #LocalHost
 port = 8899                                                            #Choosing unreserved port
@@ -10,8 +10,10 @@ server.listen()
 
 location = (host, port)
 checkstatus = server.connect_ex(location)                               #check that port is using or not
+date = datetime.datetime.now()
 if checkstatus != 0:
-    print ("Chat room server is working!")
+    print(date.strftime("%c"))
+    print ("Mara's Chatroom server is working!")
     print("Waiting for connecting\n")
 else:
     print('Sorry! Server can not run\n')
@@ -42,7 +44,28 @@ def handle(user):
             if msg=='#exit':
                 removeuser(user)
                 break
-            
+            elif msg=='#show':
+                #listnames = pickle.dumps(nicknames)
+                #user.send(pickle.loads(nicknames).encode('utf-8'))
+                listnames = "\n".join(nicknames)
+                user.send(listnames.encode('utf-8'))
+                thread = threading.Thread(target=handle, args=(user,))
+                thread.start()
+                #listnames = pickle.dumps(nicknames)
+                #user.send('SHOW'.encode('utf-8'))
+                #user.send(listnames.encode('utf-8'))
+                #listnames = str(nicknames)
+                #listnames = listnames.encode('utf-8')
+                
+                #user.send('SHOW'.encode('utf-8'))
+                #user.send(str(nicknames).encode('utf-8'))
+                #user.send('SHOW'.encode('utf-8'))
+                #user.sendto(pickle.dumps(nicknames),user)
+                #user.send('NICKNAME'.encode('utf-8'))
+                #nickname = user.recv(1024).decode('utf-8')
+                #shownames = nicknames
+                #user.send('ok')
+                break
             else:
                 broadcast(message)
                 print(str(message, 'utf-8')) #convert byte tostring   
@@ -58,28 +81,17 @@ def acceptuser():
             nickname = user.recv(1024).decode('utf-8')
             nicknames.append(nickname)
             users.append(user)
-            print("Nickname : {} ".format(nickname))                        #send in server
+            print("\nNickname : {} ".format(nickname))                        #send in server
             print("Connected with {}\n".format(str(address)))               #send in server
+            user.send('Connected to server! Let\'s chat!'.encode('utf-8'))  #send for user
             broadcast("{} joined!".format(nickname).encode('utf-8'))        #brodcast to all users
-            #user.send('Connected to server! Let\'s chat!'.encode('utf-8'))  #send msg for user
-            menulist(user)
             thread = threading.Thread(target=handle, args=(user,))
             thread.start()
     except socket.error:
         print("Shutting down")
     finally:
-        user.close()
+        user.close() 
 
-def menulist(user):
-    message = """******** Welcome to Mara's Chatroom ********
-            This is command that you can use in Chatroom
-            #help - show all menu
-            #show - for show all online users
-            #exit - exit from this Chatroom
-        ******** Hope you enjoy with my program! ********""".encode('utf-8') #Correct place!! of encode
-    user.send(message)
-
-        
 acceptuser()
 
 
