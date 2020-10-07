391c8c62d3e960bc67d344101bec3b7a0a9bbe72
pimrada-th <pimrada.pengsook@gmail.com>
2020-10-08 06:20:33 +0700

    delete b2 chatroom

--- cc.py
+++ cc.py
@@ -7,7 +7,6 @@ def menulist():
         ******** Welcome to Mara's Chatroom ********
             These are command that you can use in Chatroom
             #show or #s- for show all online users
-            #chatroom - back to chatroom
             #help or #h - show all menu
             #exit or #e- exit from this Chatroom
         ******** Hope you enjoy with my program! ********""")
@@ -57,7 +56,6 @@ def write():
             receive_thread = threading.Thread(target=receive)                  #sending messages 
             receive_thread.start()   
     
-
         elif typing == '#show' or typing == '#s': 
             user.send('#show'.encode('utf-8'))
             print("--- Online Users ---")
